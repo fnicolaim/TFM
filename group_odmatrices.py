b52167e4290350133487f2ba3cf581faa799bfbb
Francisco Nicolai <f.n.manaut@gmail.com>
2023-09-11 00:52:43 -0300

    Update group_odmatrices.py to include weather
    Weather features included in df

--- group_odmatrices.py
+++ group_odmatrices.py
@@ -8,33 +8,54 @@ import pandas as pd
 from datetime import datetime, timedelta
 from tqdm import tqdm
 
-# Generate objective dates list
-s = datetime(2021, 1, 1)
-e = datetime(2021, 6, 30)
-dates = [(s + timedelta(days=i)).strftime('%Y-%m-%d') for i in range((e - s).days + 1)]
+# Generate a list of objective dates from January 1, 2021, to June 30, 2021
+start_date = datetime(2021, 1, 1)
+end_date = datetime(2021, 6, 30)
+dates = [(start_date + timedelta(days=i)).strftime('%Y-%m-%d') for i in range((end_date - start_date).days + 1)]
 
+# Initialize an empty list to store DataFrames
 dfs = []
-for d in tqdm(dates):
-    df = pd.read_csv(os.path.join(os.getcwd(),"output",f"{d}_ttp_matrix.csv"), dtype="str")
 
-    # Format and binarize distance
+# Loop through each date in the list
+for date in tqdm(dates):
+    # Read a CSV file based on the date from the "output" directory and store it in a DataFrame
+    df = pd.read_csv(os.path.join(os.getcwd(), "output", f"{date}_ttp_matrix.csv"), dtype="str")
+
+    # Convert the 'distancia' column to float
     df.distancia = df.distancia.astype(float)
 
-    # Define the bin edges
-    bins = [0, 500, 2000, 10000, float('inf')]  # The last bin includes values >= 10000
+    # Define bin edges for distance binning
+    bins = [0, 500, 2000, 10000, float('inf')]
 
     # Define labels for the bins
     labels = ['0-500', '500-2000', '2000-10000', '10000+']
 
-    # Binarize the 'distance' column into the specified bins
+    # Binarize the 'distance' column into specified bins with labels
     df['distancia'] = pd.cut(df['distancia'], bins=bins, labels=labels, right=False)
 
+    # Convert the 'fecha' column to datetime format
     df.fecha = pd.to_datetime(df.fecha, format="%Y-%m-%d %H:%M:%S")
-    
+
+    # Add a 'trips' column with a constant value of 1
     df["trips"] = 1
-    df = df.groupby([pd.Grouper(key='fecha', freq='H'),'origen', 'destino'], as_index=False).agg({"trips":"sum"})
-    df = df.loc[df["trips"]!=0]
+
+    # Group the DataFrame by 'fecha', 'origen', and 'destino', aggregating 'trips' by sum
+    df = df.groupby([pd.Grouper(key='fecha', freq='H'), 'origen', 'destino'], as_index=False).agg({"trips": "sum"})
+
+    # Filter out rows where 'trips' is not equal to zero
+    df = df.loc[df["trips"] != 0]
+
+    # Append the processed DataFrame to the list
     dfs.append(df)
 
-df = pd.concat(dfs, ignore_index=True)
-df.to_csv(os.path.join(os.getcwd(),"output""final_odm.csv"), index=False)
+# Concatenate all DataFrames in the list into a single DataFrame, ignoring original indices
+final_df = pd.concat(dfs, ignore_index=True)
+
+# Write the final concatenated DataFrame to a CSV file in the "output" directory, excluding the index column
+final_df.to_csv(os.path.join(os.getcwd(), "output", "final_odm.csv"), index=False)
+
+# Merge with weather data
+weather_df = pd.read_csv(os.path.join(os.getcwd(), "input", "weather.txt"))
+weather_df["time"] = pd.to_datetime(weather_df["time"],format="%Y-%m-%dT%H:%M")
+final_df = pd.merge(final_df, weather_df,left_on="fecha", right_on="time",how="left").drop(columns=["time"])
+final_df.to_csv(os.path.join(os.getcwd(),"output", "final_odm_with_weather.csv"), index=False)
